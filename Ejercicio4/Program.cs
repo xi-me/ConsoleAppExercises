dcdd07ffa47decaa1ea81382cb3b5f47b02dfc3c
xi-me <xmsagas@gmail.com>
2023-08-06 15:47:17 -0600

    Add project files.

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Programa para resolver ecuaciones cuadraticas */
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Resolución de ecuaciones cuadráticas");
+        while (true)
+        {
+            Console.WriteLine("Escribe 'c' para continuar y 's' para salir:");
+            string option = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            if (option == "s")
+            {
+                break;
+            }
+            else if (option == "c")
+            {
+                double a, b, c, x1, x2;
+                Console.WriteLine("Resolución de ecuaciones de la forma: ax^2 + bx + c = 0");
+
+                Console.Write("Valor de a: ");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("Valor de b: ");
+                b = double.Parse(Console.ReadLine());
+                Console.Write("Valor de c: ");
+                c = double.Parse(Console.ReadLine());
+
+                x1 = -b + Math.Sqrt((b * b) - 4 * a * c);
+                x1 /= 2 * a;
+                x2 = -b - Math.Sqrt((b * b) - 4 * a * c);
+                x2 /= 2 * a;
+
+
+                x1 = double.Round(x1, 2);
+                x2 = double.Round(x2, 2);
+                Console.WriteLine("Resultados:");
+                Console.WriteLine($"x1 = {x1}");
+                Console.WriteLine($"x2 = {x2}");
+                Console.WriteLine();
+                Console.WriteLine("Haz enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            
+        }
+        
+    }
+}
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Programa que permite ver el contenido de un archivo plano */
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string line;
+
+        try
+        {
+            Console.WriteLine("Dirección del archivo:");
+            string path = Console.ReadLine();
+
+            StreamReader sr = new StreamReader(path);
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+        }
+    }
+}
