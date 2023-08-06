4fb7dde84ee976d0f9ee9041f866f222e24408e2
xi-me <xmsagas@gmail.com>
2023-08-06 12:11:13 -0600

    Add project files.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Crear un programa que permita ingresar el nombre, cantidad y precio de 10 productos */
+
+string productName;
+int productQuantity, maxProducts = 10;
+double productPrice, totalPricePerProduct, totalPrice = 0, IVA = 1.13;
+
+for (int i = 1; i <= maxProducts; i++)
+{
+    Console.Write($"Nombre de producto #{i}:");
+    productName = Console.ReadLine();
+
+    Console.Write($"Precio de {productName}: ");
+    productPrice = double.Parse(Console.ReadLine());
+
+    Console.Write("Cantidad: ");
+    productQuantity = int.Parse(Console.ReadLine());
+
+    totalPricePerProduct = (productQuantity * productPrice) * IVA;
+
+    totalPrice += totalPricePerProduct;
+
+    Console.Clear();
+}
+
+Console.WriteLine($"Total a pagar: ${double.Round(totalPrice, 2)}");
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Temporizador para apagar la computadora */
+
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int timeToShutDown, minutesToSeconds;
+        string cancel;
+
+        Console.WriteLine("Temporizador para apagar la computadora");
+        Console.Write("Ingrese el tiempo para apagar (minutos): ");
+        minutesToSeconds = int.Parse(Console.ReadLine());
+        timeToShutDown = minutesToSeconds * 60;
+        Process.Start("cmd.exe", $"/C shutdown -s -t {timeToShutDown}");
+        Console.WriteLine("Escribe 'c' para cancelar: ");
+        cancel = Console.ReadLine().ToLower();
+
+        if (cancel == "c")
+        {
+            Process.Start("cmd.exe", "/C shutdown -a");
+        }
+        else
+        {
+            Console.WriteLine($"La computadora se apagará en {minutesToSeconds} minutos");
+        }
+    }
+}
